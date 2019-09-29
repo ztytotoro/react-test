bf00f8207090bde0f4ecd02e5b6d3ff410369aaa
Yarn <ztytotoro@users.noreply.github.com>
2019-09-29 14:25:04 +0800

    update

--- packages/runtime/dist/types.d.ts
+++ packages/runtime/dist/types.d.ts
@@ -9,7 +9,7 @@ export interface Size {
 }
 export interface ContainerOptions extends Pos, Size {
 }
-export interface ControlOptions<T extends {} = {}> extends Pos, Size {
+export interface ControlOptions<T extends Props = {}> extends Pos, Size {
     type: string;
     props?: T;
 }
@@ -17,4 +17,7 @@ export interface Control<T extends {} = any> {
     type: string;
     render: React.FC<T>;
     option: (options: Omit<ControlOptions<T>, 'type'>) => ControlOptions<T>;
+}
+export interface Props {
+    [key: string]: string | number | boolean | Props;
 }
--- packages/runtime/src/types.ts
+++ packages/runtime/src/types.ts
@@ -12,7 +12,7 @@ export interface Size {
 
 export interface ContainerOptions extends Pos, Size {}
 
-export interface ControlOptions<T extends {} = {}> extends Pos, Size {
+export interface ControlOptions<T extends Props = {}> extends Pos, Size {
   type: string;
   props?: T;
 }
@@ -21,4 +21,8 @@ export interface Control<T extends {} = any> {
   type: string;
   render: React.FC<T>;
   option: (options: Omit<ControlOptions<T>, 'type'>) => ControlOptions<T>;
+}
+
+export interface Props {
+  [key: string]: string | number | boolean | Props;
 }
